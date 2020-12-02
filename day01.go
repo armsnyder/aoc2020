ec6022bc68877be8a3ac8b3e60b9999c19930e71
Adam Snyder <asnyder@ea.com>
2020-12-01 19:25:13 -0800

    Day 1 optimization

--- day01.go
+++ day01.go
@@ -4,20 +4,28 @@ var _ = declareDay(1, day01)
 
 func day01(partB bool, inputUntyped interface{}) interface{} {
 	input := inputUntyped.([]int)
-	for i := 0; i < len(input); i++ {
-		for j := i + 1; j < len(input); j++ {
-			if partB {
-				for k := j + 1; k < len(input); k++ {
-					if input[i]+input[j]+input[k] == 2020 {
-						return input[i] * input[j] * input[k]
-					}
-				}
-			} else {
-				if input[i]+input[j] == 2020 {
-					return input[i] * input[j]
-				}
-			}
+
+	var values [2020]bool
+	for _, value := range input {
+		if value < 2020 {
+			values[value] = true
 		}
 	}
+
+	for i, vi := range input {
+		if partB {
+			for j, vj := range input {
+				if j <= i {
+					continue
+				}
+				if vi+vj < 2020 && values[2020-vi-vj] {
+					return vi * vj * (2020 - vi - vj)
+				}
+			}
+		} else if values[2020-vi] {
+			return vi * (2020 - vi)
+		}
+	}
+
 	panic("no solution")
 }
