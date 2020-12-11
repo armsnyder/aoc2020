f2b7c4d758f4de5694245b1c54e99af8a43efffc
Adam Snyder <asnyder@ea.com>
2020-12-10 22:55:46 -0800

    Day 11 optimize

--- day11.go
+++ day11.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"io"
+	"sort"
+	"strings"
 
 	"github.com/armsnyder/aoc2020/aocutil"
 )
@@ -17,14 +19,120 @@ var _ = declareDay(11, func(part2 bool, inputReader io.Reader) interface{} {
 		count := day11CountOccupiedSeats(seats)
 
 		if lastCount == count {
-			break
+			return count
 		}
 
 		lastCount = count
 	}
-
-	return lastCount
 })
+
+func day11DoRound(seats []string, part2 bool) {
+	directions := [][2]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}
+
+	positionsToOccupy := make([][]int, len(seats))
+	positionsToEmpty := make([][]int, len(seats))
+
+	leaveSeatThreshold := 4
+	if part2 {
+		leaveSeatThreshold = 5
+	}
+
+	for i := 0; i < len(seats); i++ {
+	outer:
+		for j := 0; j < len(seats[0]); j++ {
+			switch seats[i][j] {
+			case 'L':
+				for _, dir := range directions {
+					if day11CheckOccupied(seats, i, j, dir, part2) {
+						continue outer
+					}
+				}
+
+				positionsToOccupy[i] = append(positionsToOccupy[i], j)
+
+			case '#':
+				total := 0
+
+				for _, dir := range directions {
+					if day11CheckOccupied(seats, i, j, dir, part2) {
+						total++
+
+						if total >= leaveSeatThreshold {
+							positionsToEmpty[i] = append(positionsToEmpty[i], j)
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+
+	var sb strings.Builder
+
+	for i := 0; i < len(seats); i++ {
+		if len(positionsToOccupy[i]) > 0 || len(positionsToEmpty[i]) > 0 {
+			seats[i] = day11RebuildRow(seats[i], positionsToOccupy[i], positionsToEmpty[i], &sb)
+		}
+	}
+}
+
+func day11CheckOccupied(seats []string, i, j int, dir [2]int, sightMode bool) bool {
+	for {
+		i += dir[0]
+		j += dir[1]
+
+		if !day11CheckIndex(seats, i, j) {
+			return false
+		}
+
+		switch seats[i][j] {
+		case '#':
+			return true
+		case 'L':
+			return false
+		}
+
+		if !sightMode {
+			return false
+		}
+	}
+}
+
+func day11CheckIndex(seats []string, i, j int) bool {
+	if i < 0 || i >= len(seats) {
+		return false
+	}
+
+	if j < 0 || j >= len(seats[i]) {
+		return false
+	}
+
+	return true
+}
+
+func day11RebuildRow(row string, positionsToOccupy, positionsToEmpty []int, sb *strings.Builder) string {
+	sb.Reset()
+
+	sort.Ints(positionsToOccupy)
+	sort.Ints(positionsToEmpty)
+
+	occupyI := 0
+	emptyI := 0
+
+	for i := 0; i < len(row); i++ {
+		if occupyI < len(positionsToOccupy) && positionsToOccupy[occupyI] == i {
+			sb.WriteRune('#')
+			occupyI++
+		} else if emptyI < len(positionsToEmpty) && positionsToEmpty[emptyI] == i {
+			sb.WriteRune('L')
+			emptyI++
+		} else {
+			sb.WriteByte(row[i])
+		}
+	}
+
+	return sb.String()
+}
 
 func day11CountOccupiedSeats(seats []string) int {
 	total := 0
@@ -38,92 +146,4 @@ func day11CountOccupiedSeats(seats []string) int {
 	}
 
 	return total
-}
-
-func day11DoRound(seats []string, part2 bool) {
-	directions := [][2]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}
-
-	var positionsToOccupy [][2]int
-	var positionsToEmpty [][2]int
-
-	leaveSeatThreshold := 4
-	if part2 {
-		leaveSeatThreshold = 5
-	}
-
-	for i := 0; i < len(seats); i++ {
-		for j := 0; j < len(seats[0]); j++ {
-			switch seats[i][j] {
-			case 'L':
-				ok := true
-
-				for _, dir := range directions {
-					if day11CheckOccupied(seats, i, j, dir, part2) {
-						ok = false
-						break
-					}
-				}
-
-				if ok {
-					positionsToOccupy = append(positionsToOccupy, [2]int{i, j})
-				}
-
-			case '#':
-				total := 0
-
-				for _, dir := range directions {
-					if day11CheckOccupied(seats, i, j, dir, part2) {
-						total++
-					}
-				}
-
-				if total >= leaveSeatThreshold {
-					positionsToEmpty = append(positionsToEmpty, [2]int{i, j})
-				}
-			}
-		}
-	}
-
-	for _, pos := range positionsToOccupy {
-		seats[pos[0]] = seats[pos[0]][:pos[1]] + "#" + seats[pos[0]][pos[1]+1:]
-	}
-
-	for _, pos := range positionsToEmpty {
-		seats[pos[0]] = seats[pos[0]][:pos[1]] + "L" + seats[pos[0]][pos[1]+1:]
-	}
-}
-
-func day11CheckOccupied(seats []string, i, j int, dir [2]int, sightMode bool) bool {
-	i += dir[0]
-	j += dir[1]
-
-	for day11CheckIndex(seats, i, j) {
-		switch seats[i][j] {
-		case '#':
-			return true
-		case 'L':
-			return false
-		}
-
-		if !sightMode {
-			return false
-		}
-
-		i += dir[0]
-		j += dir[1]
-	}
-
-	return false
-}
-
-func day11CheckIndex(seats []string, i, j int) bool {
-	if i < 0 || i >= len(seats) {
-		return false
-	}
-
-	if j < 0 || j >= len(seats[i]) {
-		return false
-	}
-
-	return true
 }
