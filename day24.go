c48b155e5ed77d1d54d8104f4b2cee4c64ae48d5
Adam Snyder <asnyder@ea.com>
2020-12-23 21:36:30 -0800

    Day 24 part 2

--- day24.go
+++ day24.go
@@ -14,6 +14,21 @@ var _ = declareDay(24, func(part2 bool, inputReader io.Reader) interface{} {
 })
 
 func day24Part1(inputReader io.Reader) interface{} {
+	state := day24InitialState(inputReader)
+	return day24CountBlackTiles(state)
+}
+
+func day24CountBlackTiles(state map[day24Coord]bool) int {
+	result := 0
+	for _, v := range state {
+		if v {
+			result++
+		}
+	}
+	return result
+}
+
+func day24InitialState(inputReader io.Reader) map[day24Coord]bool {
 	state := make(map[day24Coord]bool)
 	aocutil.VisitStrings(inputReader, func(v string) {
 		var pos day24Coord
@@ -24,13 +39,7 @@ func day24Part1(inputReader io.Reader) interface{} {
 			state[pos] = true
 		}
 	})
-	result := 0
-	for _, v := range state {
-		if v {
-			result++
-		}
-	}
-	return result
+	return state
 }
 
 func day24VisitDirections(v string, f func(dir day24Coord)) {
@@ -61,9 +70,47 @@ func day24VisitDirections(v string, f func(dir day24Coord)) {
 }
 
 func day24Part2(inputReader io.Reader) interface{} {
-	aocutil.VisitStrings(inputReader, func(v string) {})
+	state := day24InitialState(inputReader)
+	for i := 0; i < 100; i++ {
+		day24Process(state)
+	}
+	return day24CountBlackTiles(state)
+}
 
-	panic("no solution")
+func day24Process(blackTiles map[day24Coord]bool) {
+	whiteTiles := make(map[day24Coord]bool)
+
+	for blackTile := range blackTiles {
+		blackNeighbors := 0
+
+		for _, neighbor := range blackTile.neighbors() {
+			if blackTiles[neighbor] {
+				blackNeighbors++
+			} else {
+				whiteTiles[neighbor] = true
+			}
+		}
+
+		if blackNeighbors == 0 || blackNeighbors > 2 {
+			defer delete(blackTiles, blackTile)
+		}
+	}
+
+	for whiteTile := range whiteTiles {
+		blackNeighbors := 0
+
+		for _, neighbor := range whiteTile.neighbors() {
+			if blackTiles[neighbor] {
+				blackNeighbors++
+			}
+		}
+
+		if blackNeighbors == 2 {
+			defer func(whiteTile day24Coord) {
+				blackTiles[whiteTile] = true
+			}(whiteTile)
+		}
+	}
 }
 
 type day24Coord struct {
@@ -73,4 +120,15 @@ type day24Coord struct {
 func (c *day24Coord) add(c2 day24Coord) {
 	c.x += c2.x
 	c.y += c2.y
+}
+
+func (c day24Coord) neighbors() [6]day24Coord {
+	return [6]day24Coord{
+		{x: c.x + 1, y: c.y - 1},
+		{x: c.x, y: c.y - 1},
+		{x: c.x, y: c.y + 1},
+		{x: c.x - 1, y: c.y + 1},
+		{x: c.x + 1, y: c.y},
+		{x: c.x - 1, y: c.y},
+	}
 }
--- day24_test.go
+++ day24_test.go
@@ -36,10 +36,31 @@ wseweeenwnesenwwwswnew
 
 func TestDay24Part2(t *testing.T) {
 	runDayTests(t, 24, []dayTest{
-		//{
-		//	part2: true,
-		//	input: ``,
-		//	want:  nil,
-		//},
+		{
+			part2: true,
+			input: `
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew
+`,
+			want: 2208,
+		},
 	})
 }
